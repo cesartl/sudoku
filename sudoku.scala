32d59abe1b57c10db9bed782f0ecd19eb88c0844
avl055 <avl@gmx.co.uk>
2016-04-09 23:47:27 +0100

    fix

--- sudoku.scala
+++ sudoku.scala
@@ -5,6 +5,7 @@ case class Fixed(e:Int) extends Cell
 case class Undetermined(list:List[Int]) extends Cell
 
 
+
 val sudoku = """
    26 7 1
 68  7  9 
@@ -37,14 +38,14 @@ def myNeighbourRow(s: Vector[Vector[Cell]], p:(Int,Int), e:Int) = {
 
 def myNeighbourColumn(s: Vector[Vector[Cell]], p:(Int,Int), e:Int) = {
     val t = s.transpose
-    myNeighbourRow(t,p,e)
+    (!t(p._2).contains(Fixed(e))) || s(p._1)(p._2) == Fixed(e)
 }
 
 def myNeighbourCube(s: Vector[Vector[Cell]], p:(Int,Int), e:Int) = {
     val ci = p._1 / 3
     val cj = p._2 / 3
     val t = s.grouped(3).toList(ci).map{x=>(x.grouped(3).toList)(cj)}.flatten
-    myNeighbourRow(t,p,e)
+    (!t.contains(Fixed(e))) || s(p._1)(p._2) == Fixed(e)
 }
 
 def filter(sn: Vector[Vector[Cell]]):Vector[Vector[Cell]] = {
@@ -65,9 +66,13 @@ def filter(sn: Vector[Vector[Cell]]):Vector[Vector[Cell]] = {
         }
     }
     if(sn1==sn) sn
-    else filter(sn1)    
+    else filter(sn1)
+    //sn1
+    
 }
 
 var solved = filter(s)
+
 var printable = solved.map{_.map(_.asInstanceOf[Fixed].e).mkString("")}.mkString("\n")
+
 println(printable)
